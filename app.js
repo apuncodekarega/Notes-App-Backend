adaf4b9dba4addae55ece721375f3abb7986ab05
apuncodekarega <55999867+apuncodekarega@users.noreply.github.com>
2020-10-01 22:17:17 +0530

    Update app.js
    Changed Port

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.get('/', (req, res) => {
 require('./app/routes/note.routes.js')(app);
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(8080, () => {
     console.log("Server is listening on port 3000");
 });
